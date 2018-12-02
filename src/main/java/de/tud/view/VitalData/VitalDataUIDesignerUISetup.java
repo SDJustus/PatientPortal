576b4b56f2eeff3b2b73944db7abf1be35a632ed
Evator <saschaweliki@web.de>
2018-12-02 02:58:03 +0100

    small layout changes

--- src/main/java/de/tud/view/VitalData/VitalDataUIDesignerUISetup.java
+++ src/main/java/de/tud/view/VitalData/VitalDataUIDesignerUISetup.java
@@ -67,10 +67,10 @@ public VitalDataUIDesignerUISetup()
 
 
 //Setup Stepper
-    weightStepper.setMinValue(70.0f);
+    weightStepper.setMinValue(0.0f);
     weightStepper.setMaxValue(1000f);
     weightStepper.setStepAmount(1f);
-    weightStepper.setValue(0f);
+    weightStepper.setValue(70f);
     weightStepper.setNumberOfDecimals(2);
     weightStepper.setDescription("Bitte geben Sie Ihr Gewicht ein.");
     weightStepper.addStyleName(MaterialTheme.COMBOBOX_ALIGN_CENTER);
