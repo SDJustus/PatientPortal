6f1049abb21a03754f3146ef4ef8a5ec95d8461b
Stets42 <basti123s@googlemail.com>
2019-01-02 18:59:17 +0100

    - Started to implement Medication schedule in UI

---
+++ src/main/java/de/tud/controller/MedicationScheduleController.java
@@ -0,0 +1,16 @@
+package de.tud.controller;
+
+import de.tud.view.MedicationScheduleSetup;
+
+public class MedicationScheduleController {
+
+    MedicationScheduleSetup designerView;
+
+    public MedicationScheduleController(MedicationScheduleSetup designerView){
+        this.designerView=designerView;
+    }
+
+    public void loadMedicationSchedule(){
+
+    }
+}
