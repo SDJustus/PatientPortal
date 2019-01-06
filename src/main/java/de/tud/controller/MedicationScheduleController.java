7e0171f00924e4c98e0eb58e916cd71e93983490
Stets42 <basti123s@googlemail.com>
2019-01-07 00:20:07 +0100

    - all bugs fixed

--- src/main/java/de/tud/controller/MedicationScheduleController.java
+++ src/main/java/de/tud/controller/MedicationScheduleController.java
@@ -2,26 +2,24 @@ package de.tud.controller;
 
 import de.tud.model.manager.MedicationPlanManager;
 import de.tud.model.medication.DummyMedication;
-import de.tud.view.MedicationScheduleSetup;
+import de.tud.view.MedicationScheduleView;
 import java.util.List;
 
 public class MedicationScheduleController {
 
-    private MedicationScheduleSetup medicationScheduleSetup;
+    private MedicationScheduleView medicationScheduleView;
     MedicationPlanManager medicationPlanManager;
 
-    public MedicationScheduleController(MedicationScheduleSetup medicationScheduleSetup){
+    public MedicationScheduleController(MedicationScheduleView medicationScheduleView){
 
-        this.medicationScheduleSetup = medicationScheduleSetup;
+        this.medicationScheduleView = medicationScheduleView;
         medicationPlanManager = MedicationPlanManager.getInstance();
-        medicationScheduleSetup.getViewComponent();
-        loadMedicationSchedule();
     }
 
     public void loadMedicationSchedule(){
         try{
             List<DummyMedication> dummyMedication = medicationPlanManager.getAllDummyMedication();
-            medicationScheduleSetup.getMedicationSchedule().setItems(dummyMedication);
+            medicationScheduleView.getMedicationSchedule().setItems(dummyMedication);
 
         }
         catch(NullPointerException e){
